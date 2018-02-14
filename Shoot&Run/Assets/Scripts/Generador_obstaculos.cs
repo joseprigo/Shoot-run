24a1d45327077e47530b33e855721df254418b80
joseprigo <36240985+joseprigo@users.noreply.github.com>
2018-02-14 19:28:36 +0100

    hola

---
+++ Shoot&Run/Assets/Scripts/Generador_obstaculos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador_obstaculos : MonoBehaviour {
+
+	public GameObject[] obstaculos;
+	public float segundos;
+	float segundos_actuales;
+
+	// Use this for initialization
+	void Start () {
+		segundos_actuales = segundos;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		segundos_actuales -= Time.deltaTime;
+		if (segundos_actuales <= 0.0f) {
+			//CREAR OBJETO 
+			print("objeto nuevo añadido nuevo obstáculo");
+			Instantiate (obstaculos [Random.Range (0, obstaculos.Length)],
+				transform.position = new Vector3 (Random.Range (-6.0f, 6.0f), -14, 0),transform.rotation);
+			segundos_actuales = segundos;
+
+		}
+	}
+}
---
+++ Shoot&Run/Assets/Scripts/Scene_controler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_controler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnDie(){
+		Application.Quit ();
+	}
+}
