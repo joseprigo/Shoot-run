dbb7e040ac25e8b615db342075326ad3b1568170
joseprigo <36240985+joseprigo@users.noreply.github.com>
2018-02-21 19:35:14 +0100

    juntando primera vez

--- Shoot&Run/Assets/Scripts/Generador_obstaculos.cs
+++ Shoot&Run/Assets/Scripts/Generador_obstaculos.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Generador_obstaculos : MonoBehaviour {
 
 	public GameObject[] obstaculos;
+	public float X;
+	public float Y;
 	public float segundos;
 	float segundos_actuales;
 
@@ -21,7 +23,7 @@ public class Generador_obstaculos : MonoBehaviour {
 			//CREAR OBJETO 
 			print("objeto nuevo añadido nuevo obstáculo");
 			Instantiate (obstaculos [Random.Range (0, obstaculos.Length)],
-				transform.position = new Vector3 (Random.Range (-6.0f, 6.0f), -14, 0),transform.rotation);
+				transform.position = new Vector2 (X,Y),transform.rotation);
 			segundos_actuales = segundos;
 
 		}
