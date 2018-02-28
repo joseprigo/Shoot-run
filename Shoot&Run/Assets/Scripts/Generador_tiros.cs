8cca163733bead145d2922004fda2f3b8732bb1e
joseprigo <36240985+joseprigo@users.noreply.github.com>
2018-02-28 20:16:41 +0100

    dispara y chocsa

---
+++ Shoot&Run/Assets/Scripts/Generador_tiros.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador_tiros : MonoBehaviour {
+
+	public GameObject tiro;
+	//public float segundos;
+
+
+	// Use this for initialization
+	void Start () {
+		//segundos_actuales = segundos;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//empieza restando el tiempo actual
+		//segundos_actuales -= Time.deltaTime;
+
+		//
+		if (Input.GetKeyDown("space")) {
+			//CREAR OBJETO 
+			//print("objeto nuevo añadido nuevo obstáculo");
+			Instantiate (tiro,
+				transform.position,transform.rotation);
+			//segundos_actuales = Random.Range(1,segundos);
+
+		}
+	}
+}
