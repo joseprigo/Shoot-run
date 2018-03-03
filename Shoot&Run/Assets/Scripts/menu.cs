e8222737aaca04d6cccffe4e069efcdddfbcc8f3
Carlos Alvarez Fernandez <caf85@hotmail.com>
2018-03-03 21:06:20 +0100

    menu
    añadido transicion de menu

---
+++ Shoot&Run/Assets/Scripts/menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	void OnMouseDown () {
+	SceneManager.LoadScene (1);
+	}
+
+
+	}
+
