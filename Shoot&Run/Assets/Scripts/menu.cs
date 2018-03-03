1647f3c7a074436b7f59166619f8d8b9e216042c
Carlos Alvarez Fernandez <caf85@hotmail.com>
2018-03-03 21:11:57 +0100

    comentario
    menu.cs

--- Shoot&Run/Assets/Scripts/menu.cs
+++ Shoot&Run/Assets/Scripts/menu.cs
@@ -6,8 +6,9 @@ public class menu : MonoBehaviour {
 
 	// Use this for initialization
 
-
+	//función que se activa al hacer click con el ratón en el elemento que posee este script
 	void OnMouseDown () {
+	//Cargamos la escena que esté inmediatamente a continuación de la escena en la que se ejecute este script
 	SceneManager.LoadScene (1);
 	}
 
