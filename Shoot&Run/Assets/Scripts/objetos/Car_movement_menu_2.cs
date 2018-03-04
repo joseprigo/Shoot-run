933accf1e97a2aa336aa77b7ba1ce38553ee2201
Carlos Alvarez Fernandez <caf85@hotmail.com>
2018-03-04 19:40:02 +0100

    prefabs
    añadiendo png y prefabs

---
+++ Shoot&Run/Assets/Scripts/objetos/Car_movement_menu_2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_movement_menu : MonoBehaviour
+{
+
+	public float speed;
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x - speed * Time.deltaTime,
+			transform.position.y,
+			transform.position.z);
+	}
+
+}
