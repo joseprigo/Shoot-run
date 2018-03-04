1ce0f595c47007f261c2045f96cacd71a149104c
joseprigo <36240985+joseprigo@users.noreply.github.com>
2018-03-04 21:56:38 +0100

    Scripts para mostrar la puntuacion

--- Shoot&Run/Assets/Scripts/Estado_juego.cs
+++ Shoot&Run/Assets/Scripts/Estado_juego.cs
@@ -7,25 +7,34 @@ public class Estado_juego : MonoBehaviour {
 	public float gasolinaInicial = 100.0f;
 	public int vida;
 	public float gasolina;
-	private Scene_controler Scene;
+	private Scene_controler Scene; //obtener las funciones del Scene_controler.cs
+	private Text_controller Text; //obtener las funciones del Text_controler.cs
 	private const float MAX_GAS = 200.0f;
 	private const int MAX_VIDA = 100;
 
 
+	int tiempo = 0;
 	float periodoGasolina = 5;
 
 	void Start () {
-		Scene = Scene_controler.ObtenerComponente<Scene_controler> ("Scene_controller");
+		Scene = Scene_controler.ObtenerComponente<Scene_controler> ("Scene_controller"); //inicializar las funciones de la escena
+		Text = Scene_controler.ObtenerComponente<Text_controller>("Scene_controller");
 		vida = vidaInicial;
+
 		gasolina = gasolinaInicial;
+		Text.setEST (vida);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//tiempo trancurrido en el juego
+		tiempo += (int) (Time.deltaTime*100.0f);
+		print (Time.deltaTime);
+		Text.setTmp (tiempo);
 
 		//Cada 5 segundos se baja la gasolina aunque no haya movimiento
 		periodoGasolina-= Time.deltaTime;
-
+		Text.setGAS (gasolina);
 		if (periodoGasolina <= 0.0f) {
 			gasolina--;
 			periodoGasolina = 5;
@@ -51,6 +60,7 @@ public class Estado_juego : MonoBehaviour {
 		
 		if (vida > 0) {
 			vida-= 10;
+			Text.setEST (vida);
 			print ("Chocar: " + vida); 
 		}
 		if (vida <= 0) {
@@ -65,6 +75,7 @@ public class Estado_juego : MonoBehaviour {
 		if (gasolina > MAX_GAS) {
 			gasolina = MAX_GAS;
 		}
+		Text.setGAS (gasolina);
 		print ("Repostando: " + gasolina); 
 	}
 
@@ -73,6 +84,7 @@ public class Estado_juego : MonoBehaviour {
 		if (vida > MAX_VIDA) {
 			vida = MAX_VIDA;
 		}
+		Text.setEST (vida);
 		print ("Reparación: " + vida);
 	}
 
